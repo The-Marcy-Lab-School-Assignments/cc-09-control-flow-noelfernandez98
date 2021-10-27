69c1107d16c3946ee2b68eb07d3f16027af093aa
Noel Fernandez <noelfernandez98@gmail.com>
2021-10-27 14:48:12 +0000

    code challenge 10/27

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
-//1
-function betweenFiveAndTen(){
-  
-}
-
+// //1
+// function betweenFiveAndTen(num){
+//   if(num > 10){
+//     return false
+//   }else if(num < 5){
+//       return false
+//   }else{
+//       return true
+//   }
+// }
+// console.log(betweenFiveAndTen(5))
 //2
-function betweenTenAndThirty(){
-  
-}
-
+// function betweenTenAndThirty(num){
+//   if(num > 30){
+//       return false
+//     }else if(num < 10){
+//       return false
+//     }else{
+//         return true
+//     }
+// }
+// console.log(betweenTenAndThirty(11))
 //3
-function betweenThirtyAndFifty(){
-  
-}
-
+// function betweenThirtyAndFifty(num){
+//   if(num <= 50 && num >= 30){
+//       return true
+//   }else{
+//       return false
+//   }
+// }
+// console.log(betweenThirtyAndFifty(45))
 
 //4
-function betweenThirtyFiftySeventy(){
-  
+function betweenThirtyFiftySeventy(num){
+  if(num <= 50 && num >= 30){
+       return true
+}else if(num <= 70 && num >= 50){
+       return true
+}else{
+    return false
 }
-
+}
+console.log(betweenThirtyFiftySeventy(50))
 //5
-function betweenTwentyFiftySixty(){
-  
+function betweenTwentyFiftySixty(num){
+   if(num <= 20 && num >= 0){
+       return true
+}else if(num <= 60 && num >= 50){
+       return true
+}else{
+    return false
+}
 }
 
+console.log(betweenTwentyFiftySixty(45))
 //6
-function betweenTenTwentyThirty(){
-  
+function betweenTenTwentyThirty(num){
+    if(num <= 10 && num >= 0){
+       return true
+}else if(num <= 30 && num >= 20){
+       return true
+}else{
+    return false
+} 
 }
+console.log(betweenTenTwentyThirty(45))
